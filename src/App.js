07ac8fc48b865d93a1116cf5da1e3dbb65409120
Tomas Caira Boronat <tomascaira8@gmail.com>
2022-05-25 16:58:31 +0200

    subida app para desafio

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h2>Las ofertas de la semana</h2>
         <h1>Cd Romance</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
