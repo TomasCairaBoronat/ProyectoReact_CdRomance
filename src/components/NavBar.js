42728b2e010e123e62ae7b98e4b2b9adf326ae06
Tomas Caira Boronat <tomascaira8@gmail.com>
2022-06-01 17:35:24 +0200

    arreglo sintactico de react

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,17 @@
 import NavLogo from '../images/logo.png';
 import CartWidget from './CartWidget.js';
-import NavLinks from './NavLinks';
 import '../styles/nav-bar.css';
+import NavLinks from './NavLinks';
 function NavBar() {
     return(
-      <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
         <a className="navbar-brand" href="./index.html"><img src={NavLogo} id="logoNav" alt='logo of CD Romance'/></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <NavLinks hrefA="./index.html" title="Home"/>
             <NavLinks hrefA="#" title="Categories"/>
             <NavLinks hrefA="#" title="Contact"/>
