a12af176b59a9d0011ed947fbd6f2a9d26d7a99a
Tomas Caira Boronat <tomascaira8@gmail.com>
2022-06-10 00:30:07 +0200

    adicion de onAdd

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,25 +1,30 @@
 import React, {useState} from "react";
 function ItemCount({stock,initial}){
     const [count,setCount] = useState(initial)
-    function add (){
+    const add = () => {
         if(count < stock){
 
             setCount(count + 1)
             
         }
     }
-    function minus(){
+    const minus = () => {
         if (count > initial){
 
             setCount(count - 1)
 
         }
     }
+    const onAdd = () => {
+        console.log(count)
+    }
     return(
         <div className="container">
             <input onClick={minus} type="button" value="-" />
             <span>{count}</span>
             <input onClick={add} type="button" value="+" />
+            <input onClick={onAdd} type="button" value="Agregar al carrito" />
+
         </div>
     )
 }
